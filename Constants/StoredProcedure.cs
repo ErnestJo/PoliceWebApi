11bea3c757a018909f664f485ffa81a7b54325b2
ErnestJo <smartlabstech@yahoo.com>
2022-10-09 16:29:07 +0300

    staff management stored procedure

--- Constants/StoredProcedure.cs
+++ Constants/StoredProcedure.cs
@@ -13,5 +13,9 @@ namespace policeWebApi.Constants
         public static string getGender = schema + "[get_gender]";
         public static string getMartialStatus = schema + "[get_martial]";
         public static string getTitle = schema + "[get_title]";
-    }
+
+        public static string addStaff = schema + "[add_staffs]";
+        public static string getStaff = schema + "[get_staffs]";
+        public static string updateStaff = schema  + "[update_staff]"
+;    }
 }
