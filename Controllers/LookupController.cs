4268c29839c94feefe137f516f1aa5c8aafcd836
ErnestJo <smartlabstech@yahoo.com>
2022-10-09 15:46:42 +0300

    repository stored procedure lookup

--- Controllers/LookupController.cs
+++ Controllers/LookupController.cs
@@ -21,12 +21,73 @@ namespace policeWebApi.Controllers
         {
             _spconn = spconn;
         }
+        
         [HttpGet(nameof(GetRoles))]
         public async Task<ResponseLookup> GetRoles()
         {
             try
             {
                 var results = await _spconn.roleLookup();
+                return results;
+            }
+            catch (Exception ex)
+            {
+                ResponseLookup exp = new ResponseLookup
+                {
+                    Code = Codes.General_failure,
+                    Message = ex.Message
+                };
+                //log error
+                return exp;
+            }
+        }
+
+        [HttpGet(nameof(GetGender))]
+        public async Task<ResponseLookup> GetGender()
+        {
+            try
+            {
+                var results = await _spconn.genderLookup();
+                return results;
+            }
+            catch (Exception ex)
+            {
+                ResponseLookup exp = new ResponseLookup
+                {
+                    Code = Codes.General_failure,
+                    Message = ex.Message
+                };
+                //log error
+                return exp;
+            }
+        }
+
+        [HttpGet(nameof(GetTitle))]
+        public async Task<ResponseLookup> GetTitle()
+        {
+            try
+            {
+                var results = await _spconn.titleLookup();
+                return results;
+            }
+            catch (Exception ex)
+            {
+                ResponseLookup exp = new ResponseLookup
+                {
+                    Code = Codes.General_failure,
+                    Message = ex.Message
+                };
+                //log error
+                return exp;
+            }
+        }
+
+        [HttpGet(nameof(GetMartialStatus))]
+        public async Task<ResponseLookup> GetMartialStatus()
+        {
+            try
+            {
+                var results = await _spconn.martialStatusLookup();
                 return results;
             }
             catch (Exception ex)
