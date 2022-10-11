08a580e6c7f8f669cd0e5802e48de694b1121690
ErnestJo <smartlabstech@yahoo.com>
2022-10-11 19:26:11 +0300

    login Api

--- Constants/StoredProcedure.cs
+++ Constants/StoredProcedure.cs
@@ -9,13 +9,18 @@ namespace policeWebApi.Constants
     {
         private static string schema = "[dbo].";
 
+        // Lookup
         public static string getRoles = schema + "[get_role]";
         public static string getGender = schema + "[get_gender]";
         public static string getMartialStatus = schema + "[get_martial]";
         public static string getTitle = schema + "[get_title]";
 
+        //staff managament
         public static string addStaff = schema + "[add_staffs]";
         public static string getStaff = schema + "[get_staffs]";
-        public static string updateStaff = schema  + "[update_staff]"
-;    }
+        public static string updateStaff = schema  + "[update_staff]";
+
+        // staff account 
+        public static string login = schema + "[staff_login]";
+    }
 }
---
+++ Model/Account/rq/AccountRq.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace policeWebApi.Model.Account.rq
+{
+    public class AccountRq
+    {
+    }
+
+    public class LoginRq
+    {
+        public string staff_username { get; set; }
+
+        public string staff_password { get; set; }
+    }
+}
